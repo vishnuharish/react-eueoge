6094c87a861d8a677b4e64a3bf1d20fa9575ae6e
vishnuharish <vishnuharish12@gmail.com>
2020-02-14 16:37:08 +0530

    hiding the codes

--- database/config.js
+++ database/config.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 const firebaseConfig = {
-    apiKey: "AIzaSyAoZxbcq7F6jxz4rvVMCNGpbKSLdSU_Uzg",
-    authDomain: "photowall-26636.firebaseapp.com",
-    databaseURL: "https://photowall-26636.firebaseio.com",
-    projectId: "photowall-26636",
-    storageBucket: "photowall-26636.appspot.com",
-    messagingSenderId: "708575166189",
-    appId: "1:708575166189:web:ec05255a03318dfa20d6e4"
+    apiKey: "***",
+    authDomain: "***",
+    databaseURL: "***",
+    projectId: "***",
+    storageBucket: "***",
+    messagingSenderId: "***",
+    appId: "***"
   };
 
   firebase.initializeApp(firebaseConfig);
